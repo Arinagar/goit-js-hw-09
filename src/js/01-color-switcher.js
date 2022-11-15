27489160ca782bbb91ed3d60ca3d59a4387fe79a
Mark Shliakhov <valmarksh@gmail.com>
2022-11-15 17:55:09 +0200

    task 1 finish

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,23 @@
+const buttonStartEl = document.querySelector('button[data-start]');
+const buttonStopEl = document.querySelector('button[data-stop]');
+let colorChange = null;
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function onButtonStartClick(event) {
+  colorChange = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+
+  event.target.disabled = !buttonStopEl.disabled;
+}
+
+function onButtonStopClick(event) {
+  clearInterval(colorChange);
+  buttonStartEl.disabled = false;
+}
+
+buttonStartEl.addEventListener('click', onButtonStartClick);
+buttonStopEl.addEventListener('click', onButtonStopClick);
