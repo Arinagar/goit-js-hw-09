af46d26424e7cd1953c14a13d5b145b60725458e
Mark Shliakhov <valmarksh@gmail.com>
2022-11-16 00:43:45 +0200

    some changes in 3 task

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,18 @@
+import Notiflix from 'notiflix';
+
+const formEl = document.querySelector('.form');
+
+function onSubmitCreatePromise(event) {
+  event.preventDefault();
+  createPromise();
+}
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
-    // Fulfill
+    Notiflix.Notify.success('Great');
   } else {
-    // Reject
+    Notiflix.Notify.failure('Bad');
   }
 }
+
+formEl.addEventListener('submit', onSubmitCreatePromise);
